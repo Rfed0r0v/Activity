539a44ac9051773a1049075fdbfc21e7415c15b4
Rfed0r0v <rf.rf944@gmail.com>
2022-08-25 20:05:08 +0300

    work

--- app/src/main/java/com/example/activity/MainActivity2.kt
+++ app/src/main/java/com/example/activity/MainActivity2.kt
@@ -1,24 +1,34 @@
 package com.example.activity
 
-import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+
 
 class MainActivity2 : AppCompatActivity(R.layout.activity_main2) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val editTextPhone2 = findViewById<TextView>(R.id.editTextPhone2)
-        val btPhoneButton2 = findViewById<ImageButton>(R.id.btPhoneButton2)
+        val btCallButton2 = findViewById<ImageButton>(R.id.btPhoneButton2)
+        var data: String? = ""
 
-        btPhoneButton2.setOnClickListener {
-            setResult(Activity.RESULT_OK, Intent().apply {
-                putExtra("result", "1010101")
-            })
-            finish()
+        val extras = intent.extras
+        if (extras != null) {
+            data = extras.getString("result")
+            if (data != null) {
+                editTextPhone2.text = data
+            }
+
+        }
+
+        btCallButton2.setOnClickListener {
+            val intent = Intent(Intent.ACTION_CALL)
+            intent.data = Uri.parse("tel:$data")
+            startActivity(intent)
         }
     }
 }
