f97c377aaea9be1b551c580fe72fd6804ca5316a
Rfed0r0v <rf.rf944@gmail.com>
2022-08-25 21:25:07 +0300

    work3

--- app/src/main/java/com/example/activity/MainActivity2.kt
+++ app/src/main/java/com/example/activity/MainActivity2.kt
@@ -1,11 +1,15 @@
 package com.example.activity
 
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 
 class MainActivity2 : AppCompatActivity(R.layout.activity_main2) {
@@ -28,7 +32,25 @@ class MainActivity2 : AppCompatActivity(R.layout.activity_main2) {
         btCallButton2.setOnClickListener {
             val intent = Intent(Intent.ACTION_CALL)
             intent.data = Uri.parse("tel:$data")
-            startActivity(intent)
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.CALL_PHONE
+                )
+                != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf(Manifest.permission.CALL_PHONE),
+                    2
+                )
+
+            } else {
+                try {
+                    startActivity(intent)
+                } catch (e: SecurityException) {
+                    e.printStackTrace()
+                }
+            }
         }
     }
 }
